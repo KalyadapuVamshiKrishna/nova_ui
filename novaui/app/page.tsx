b5c271194d49dbea4f7ac6269dd5655b6acf6ca9
Vamshi Krishna <curiuskrish@.com>
2025-09-16 15:25:09 +0530

    first commit

---
+++ novaui/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+   <h1 className="text-2xl ">Hi there! How are you?</h1>
+   <p>This is going to be my UI library.</p>
+   <button
+   
+   </>
+  );
+}
